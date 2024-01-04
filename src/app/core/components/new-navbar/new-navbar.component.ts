e7c0cc58e96922e1b1bf9fdcc9ff5a9b2946e3c2
Kritartha Nath <2000108446@hexaware.com>
2024-01-04 14:56:38 +0530

    nav

---
+++ src/app/core/components/new-navbar/new-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-navbar',
+  templateUrl: './new-navbar.component.html',
+  styleUrls: ['./new-navbar.component.css']
+})
+export class NewNavbarComponent {
+
+}
